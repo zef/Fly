d03af9e35b46b5215e2d34cf123ebdc9e4a2f589
Zef Houssney <zef@madebykiwi.com>
2016-01-02 21:19:06 -0700

    Bring in new routing from playground.

--- Sources/FlyRouter.swift
+++ Sources/FlyRouter.swift
@@ -1,24 +1,19 @@
 typealias FlyAction = (FlyRequest, FlyResponse) -> FlyResponse
 
-struct FlyRoute {
-    let path: String
-    let method: HTTPMethod
-    let action: FlyAction
-}
-
 // would be cool to be able to nest routers
 // maybe even have a pattern where a controller defines a router, and then nests it at a mount point?
 // that would make refactoring routes really easy
+// not sure if it's better to generate flat routes as they are added somehow,
+// or to have a nested scheme where the nesting is calculated at matching time...
 struct FlyRouter {
-    var routes = [FlyRoute]()
 
-    mutating func GET(path: String, action: FlyAction) {
-        self.route(path, method: .GET, action: action)
+    var routes = [Routable]()
+
+    mutating func register(routes: Routable...) {
+        register(routes)
     }
-
-    mutating func route(path: String, method: HTTPMethod = .GET, action: FlyAction) {
-        let route = FlyRoute(path: path, method: method, action: action)
-        routes.append(route)
+    mutating func register(routes: [Routable]...) {
+        self.routes.appendContentsOf(routes.flatMap{$0})
     }
 
     func handle(request: FlyRequest) -> FlyResponse {
@@ -29,16 +24,56 @@ struct FlyRouter {
     }
 
     // if path matches but not method, could suggest that to warn the dev about the potential problem
-    func matchingRoute(request: FlyRequest) -> FlyRoute? {
-        return routes.filter { route in
-            request.method == route.method &&
-            request.path == route.path
-        }.first
+    func matchingRoute(request: FlyRequest) -> Routable? {
+        for route in routes {
+            if route.matches(request) {
+                return route
+            }
+        }
+        return nil
     }
 
     var friendlyRouteList: String {
         let list = routes.map { return "\($0.method) \($0.path)" }
         return list.joinWithSeparator("\n")
+    }
+}
+
+extension FlyRouter {
+    struct Route: Routable {
+        let path: String
+        let method: HTTPMethod
+        let action: FlyAction
+    }
+
+    mutating func route(path: String, method: HTTPMethod = .GET, action: FlyAction) {
+        let route = Route(path: path, method: method, action: action)
+        register(route)
+    }
+
+    mutating func GET(path: String, action: FlyAction) {
+        self.route(path, method: .GET, action: action)
+    }
+}
+
+protocol Routable {
+    var path: String { get }
+    var action: FlyAction { get }
+
+    // default implementation returns .GET
+    var method: HTTPMethod { get }
+}
+
+extension Routable {
+    var method: HTTPMethod { return .GET }
+    //    var actions: [Routable.Type] { return [] }
+
+    func matchesPath(path: String) -> Bool {
+        return self.path == path
+    }
+
+    func matches(request: FlyRequest) -> Bool {
+        return method == request.method && matchesPath(request.path)
     }
 }
 
