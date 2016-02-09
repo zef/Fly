bf330d0b010bbe9125b48018730b01d03fd86942
Zef Houssney <zef@madebykiwi.com>
2016-02-09 00:58:44 -0700

    Work on command line tool for running server and outputting logs as it runs

--- command/main.swift
+++ command/main.swift
@@ -1,33 +1,56 @@
 import Foundation
 
-func run(command: String) {
-    print("Executing command \"\(command)\":")
+func run(commands: String...) {
+    print("Executing command \"\(commands.joinWithSeparator(" "))\":")
     let task = NSTask()
-    task.launchPath = "/bin/sh"
-    task.arguments = ["-c", command]
+    // task.launchPath = "/bin/sh"
+    // task.arguments = ["-c", command]
+    task.launchPath = "/usr/bin/env"
+    task.arguments = commands
+
 
     let pipe = NSPipe()
     task.standardOutput = pipe
+
     task.launch()
 
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
-        print(output)
-    }
+    let fileHandle = pipe.fileHandleForReading
 
-    task.waitUntilExit()
+   fileHandle.waitForDataInBackgroundAndNotify()
+
+   var observer: NSObjectProtocol!
+   observer = NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: fileHandle, queue: nil) {  notification -> Void in
+       let data = fileHandle.availableData
+       if data.length > 0 {
+           if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
+               print(output, terminator: "")
+           }
+           fileHandle.waitForDataInBackgroundAndNotify()
+       } else {
+           // print("Landed", observer)
+           NSNotificationCenter.defaultCenter().removeObserver(observer)
+       }
+   }
+
+    // task.waitUntilExit()
+
     // let status = task.terminationStatus
     // print(status)
 }
 
+
 enum Command: String {
     case server
+    case docker
     case clean
     case help
 
     func execute() {
         switch self {
         case .server:
+            run("swift", "build")
+            run(".build/debug/Fly")
+        case .docker:
             run("docker rm swift")
             run("docker build -t swift ./")
             run("docker run -p 80:8080 --name swift -it swift /bin/bash")
@@ -37,6 +60,7 @@ enum Command: String {
             print("help")
         }
     }
+
 }
 
 let arguments = Process.arguments[1..<Process.arguments.count]
@@ -49,7 +73,6 @@ if let command = Command(rawValue: commandString) {
     Command.help.execute()
 }
 
-
-
+CFRunLoopRun()
 
 
