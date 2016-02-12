4d6bdbbfc7b759f42bc6ab7cc31b776614143e6c
Zef Houssney <zef@madebykiwi.com>
2016-02-11 22:05:48 -0700

    Improve fly command line tool with distinction of long running vs serial tasks.

--- command/main.swift
+++ command/main.swift
@@ -1,7 +1,8 @@
 import Foundation
 
-func run(commands: String...) {
+func run(commands: String..., longRunning: Bool = false) {
     print("Executing command \"\(commands.joinWithSeparator(" "))\":")
+
     let task = NSTask()
     // task.launchPath = "/bin/sh"
     // task.arguments = ["-c", command]
@@ -13,26 +14,31 @@ func run(commands: String...) {
     task.standardOutput = pipe
 
     task.launch()
-
     let fileHandle = pipe.fileHandleForReading
 
-   fileHandle.waitForDataInBackgroundAndNotify()
+    if longRunning {
+        fileHandle.waitForDataInBackgroundAndNotify()
+        var observer: NSObjectProtocol!
+        observer = NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: fileHandle, queue: nil) {  notification -> Void in
+            let data = fileHandle.availableData
+            if data.length > 0 {
+               if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                   print(output, terminator: "")
+               }
+               fileHandle.waitForDataInBackgroundAndNotify()
+            } else {
+               // print("Landed", observer)
+               NSNotificationCenter.defaultCenter().removeObserver(observer)
+            }
+        }
+    } else {
+        if let output = NSString(data: fileHandle.availableData, encoding: NSUTF8StringEncoding) {
+            print(output, terminator: "")
+        }
+        task.waitUntilExit()
+    }
 
-   var observer: NSObjectProtocol!
-   observer = NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: fileHandle, queue: nil) {  notification -> Void in
-       let data = fileHandle.availableData
-       if data.length > 0 {
-           if let output = NSString(data: data, encoding: NSUTF8StringEncoding) {
-               print(output, terminator: "")
-           }
-           fileHandle.waitForDataInBackgroundAndNotify()
-       } else {
-           // print("Landed", observer)
-           NSNotificationCenter.defaultCenter().removeObserver(observer)
-       }
-   }
-
-    // task.waitUntilExit()
+    print("end of command \"\(commands.joinWithSeparator(" "))\"")
 
     // let status = task.terminationStatus
     // print(status)
@@ -49,22 +55,23 @@ enum Command: String {
         switch self {
         case .server:
             run("swift", "build")
-            run(".build/debug/Fly")
+            run(".build/debug/Fly", longRunning: true)
+            CFRunLoopRun()
         case .docker:
             run("docker rm swift")
             run("docker build -t swift ./")
             run("docker run -p 80:8080 --name swift -it swift /bin/bash")
         case .clean:
-            run("swift build --clean")
+            run("swift", "build", "--clean")
         case .help:
-            print("help")
+            print("Help.")
+            print("I should really find a nice command line lib to help me generate this...")
         }
     }
 
 }
 
 let arguments = Process.arguments[1..<Process.arguments.count]
-// print(arguments)
 let commandString = arguments.first ?? "help"
 
 if let command = Command(rawValue: commandString) {
@@ -72,7 +79,5 @@ if let command = Command(rawValue: commandString) {
 } else {
     Command.help.execute()
 }
-
-CFRunLoopRun()
 
 
