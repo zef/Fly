e47d85f3f7186f26fe127b5ace667e89adf3e7b8
Zef Houssney <zef@madebykiwi.com>
2016-02-18 22:15:21 -0700

    Create Xcode project

---
+++ Xcode/Fly/Fly.h
@@ -0,0 +1,19 @@
+//
+//  Fly.h
+//  Fly
+//
+//  Created by Zef Houssney on 2/18/16.
+//  Copyright © 2016 Zef. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Fly.
+FOUNDATION_EXPORT double FlyVersionNumber;
+
+//! Project version string for Fly.
+FOUNDATION_EXPORT const unsigned char FlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fly/PublicHeader.h>
+
+
