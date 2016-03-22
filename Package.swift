6092510d4e862d7f18aa0eb1763f80ed784067a0
Zef Houssney <zef@madebykiwi.com>
2016-03-22 00:22:49 -0600

    Update HTTP4SwiftServer implementation

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         // .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
         // .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0)
         // .Package(url: "https://github.com/Zewo/Epoch.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 0, 6))
+        .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 3, 0))
     ]
 )
 
--- Sources/HTTP4SwiftServer.swift
+++ Sources/HTTP4SwiftServer.swift
@@ -1,44 +1,55 @@
-import struct http4swift.HTTPRequest
-import struct http4swift.HTTPServer
-import struct http4swift.SocketAddress
-import struct http4swift.Socket
+import http4swift
+import Nest
+
+extension String: PayloadType {
+    public mutating func next() -> [UInt8]? {
+        if self.isEmpty {
+            return nil
+        } else {
+            let bytes = [UInt8](self.utf8)
+            self = ""
+            return bytes
+        }
+    }
+}
+
+struct NestResponse: ResponseType {
+    var headers: [Header] { return [] }
+    var body: PayloadType?
+    var statusLine: String
+}
+
+extension FlyResponse {
+    var nestResponse: NestResponse {
+        return NestResponse(body: body, statusLine: "\(status.rawValue) OK")
+    }
+}
 
 struct HTTP4SwiftServer: FlyServer {
     static func start(app: FlyApp, port: Int) {
-        let address = SocketAddress(port: UInt16(port))
-        guard let sock = Socket(), server = HTTPServer(socket: sock, addr: address) else { return }
+        let httpApp: Application = { (httpRequest) -> ResponseType in
+            let path = httpRequest.path
+            let method = HTTPMethod(rawValue: httpRequest.method) ?? .GET
 
-        server.serve { (request, writer) in
-            let (statusCode, statusText, body) = self.dataForRequest(app, request: request)
+            let request = FlyRequest(path, method: method)
+            var response = app.router.handle(request)
 
-            let bytes = [UInt8](body.utf8)
-            let size = bytes.filter({ $0 != 0 }).count
-            try writer.write("HTTP/1.0 \(statusCode) \(statusText)\r\n")
-            try writer.write("Content-Length: \(size)\r\n")
-        //     for header in response!.headers {
-        //         try writer.write("\(header.0): \(header.1)\r\n")
-        //     }
-            try writer.write("\r\n")
-            try writer.write(body)
-        }
-    }
-
-    static func dataForRequest(app: FlyApp, request httpRequest: http4swift.HTTPRequest) -> (Int, String, String) {
-        let path = httpRequest.path
-        let method = HTTPMethod(rawValue: httpRequest.method) ?? .GET
-
-        let request = FlyRequest(path, method: method)
-        var response = app.router.handle(request)
-
-        if response.status == .NotFound {
-            if app.config.showDebugRoutes {
-                response.body = app.router.HTMLRouteList
-            } else {
-                response.body = "Page Not Found"
+            if response.status == .NotFound {
+                if app.config.showDebugRoutes {
+                    response.body = app.router.HTMLRouteList
+                } else {
+                    response.body = "Page Not Found"
+                }
             }
+            app.logRequest(request, response: response)
+            return response.nestResponse
         }
-        app.logRequest(request, response: response)
-        return (response.status.rawValue, "OK", response.body)
+
+        guard let server = HTTPServer(port: UInt16(port)) else {
+            App.log("HTTPServer could not be started...")
+            return
+        }
+        server.serve(httpApp)
     }
 
 }
