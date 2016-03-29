60307652f9cd3d21db8d0393c0e7e9f0ff59dbf6
Zef Houssney <zef@madebykiwi.com>
2016-03-28 23:11:09 -0600

    Remove sample app code

--- Sources/main.swift
+++
@@ -1,7 +0,0 @@
-App.log("Let's fly 🛩")
-
-let app = App(config: Config())
-let server: FlyServer.Type = HTTP4SwiftServer.self
-// let server: FlyServer.Type = EpochServer.self
-server.start(app, port: 8080)
-
