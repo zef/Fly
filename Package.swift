fe05c14db4f6c6247433703d45a0947c8cf9fc07
Zef Houssney <zef@madebykiwi.com>
2016-03-29 00:05:49 -0600

    Remove bundled servers

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
         .Package(url: "https://github.com/zef/AirTrafficController.git", Version(0, 0, 2)),
         .Package(url: "https://github.com/zef/HTTPStatus.git", Version(0, 0, 3)),
 
-        .Package(url: "https://github.com/takebayashi/http4swift.git", Version(0, 3, 0)),
+        // .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0)
+        // .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0, minor: 4)
 
         // .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 1),
         // .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 1),
